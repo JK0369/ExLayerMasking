adff44cc8ee670d00ba4a61cee65a419f9cb3815
jake.k <jake.k@hpcnt.com>
2022-04-14 21:59:58 +0900

    Implement

--- ExLayerMasking/ViewController.swift
+++ ExLayerMasking/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  private lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "마스킹 예제"
+    label.translatesAutoresizingMaskIntoConstraints = false
+    self.view.addSubview(label)
+    return label
+  }()
+  private lazy var sampleView: SampleView = {
+    let view = SampleView()
+    self.view.addSubview(view)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    NSLayoutConstraint.activate([
+      self.sampleView.heightAnchor.constraint(equalToConstant: 100),
+      self.sampleView.widthAnchor.constraint(equalToConstant: 200),
+      self.sampleView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+      self.sampleView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+    ])
+    NSLayoutConstraint.activate([
+      self.titleLabel.bottomAnchor.constraint(equalTo: self.sampleView.topAnchor, constant: -16),
+      self.titleLabel.centerXAnchor.constraint(equalTo: self.sampleView.centerXAnchor),
+    ])
   }
-
-
 }
 
+final class SampleView: UIView {
+  private enum Constants {
+    static let lineWidth = 2.0
+    static let cornerRadius = 10.0
+  }
+  init() {
+    super.init(frame: .zero)
+    self.backgroundColor = .systemBlue
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    let shapeLayer = CAShapeLayer()
+    shapeLayer.lineWidth = Constants.lineWidth
+    shapeLayer.cornerRadius = Constants.cornerRadius
+    shapeLayer.strokeColor = UIColor.black.cgColor
+    shapeLayer.fillColor = UIColor.clear.cgColor
+    shapeLayer.path = UIBezierPath(
+      roundedRect: self.bounds.insetBy(dx: Constants.lineWidth, dy: Constants.lineWidth),
+      cornerRadius: Constants.cornerRadius
+    ).cgPath
+    
+    self.layer.mask = shapeLayer
+  }
+}
